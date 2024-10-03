c46e3c4059aa6ceef6c922736d8ac770a4a690a1
anischenko alexander <a.anischenko.de@gmail.com>
2024-10-03 17:59:55 +0200

    Home-work-19

---
+++ HOME_WORK_19/index.js
@@ -0,0 +1,26 @@
+const getJokeButton = document.querySelector("#get-joke-button");
+const dataContainer = document.querySelector(".data-container");
+
+const getJoke = async() => {
+    try{
+    const response = await fetch(
+        "https://official-joke-api.appspot.com/random_joke",
+    );
+    console.log(response);
+    const result = await response.json();
+    console.log(result);
+
+    if(response.ok) {
+        dataContainer.textContent = `${result.setup} - ${result.punchline}`
+    } else {
+        throw Object.assign(new Error("Some error"), {responseError: result});
+    }
+} catch (error) {
+    dataContainer.classList.add("error");
+    dataContainer.textContent = error.responseError.message;
+};
+    
+    
+};
+
+getJokeButton.addEventListener('click', getJoke);
